3efc451bedf73cab3bb008fefd4c375e0ef39742
Andrew Kloecker <andrew.kloecker@MacBook-Pro.local>
2020-05-09 22:25:30 -0400

    lab complete

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,32 @@
 # code genre here
+class Genre
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def songs
+    Song.all.select { |song| song.genre == self }
+  end
+
+  def artists
+    Artist.all.select { |artist| artist.genres.include? self }
+  end
+
+  class << self
+    def all
+      @@all
+    end
+
+    def count
+      @@all.count
+    end
+
+    def reset_genres
+      @@all.clear
+    end
+  end
+end
--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,13 @@
-# code song here
+class Song
+  attr_accessor :name, :genre, :artist
+
+  @@all = []
+
+  def initialize
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+end
