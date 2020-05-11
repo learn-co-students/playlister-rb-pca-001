557be46e63339c5dd8041ec5492df1e92ad988b6
Pat McClernan <patmccler@gmail.com>
2020-05-11 09:37:24 -0400

    builds out song artist genre classes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,1 +1,36 @@
-# code artist here
+class Artist
+  attr_accessor :name
+  @all = []
+
+  def initialize
+    self.class.all << self
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  class << self
+    attr_reader :all
+  end
+
+  def songs
+    Song.all.select { |song| song.artist == self }
+  end
+
+  def genres
+    songs.map(&:genre).uniq
+  end
+
+  def songs=(songs)
+    songs.each { |song| song.artist = self }
+  end
+
+  def self.count
+    all.count
+  end
+
+  def self.reset_artists
+    @all = []
+  end
+end
--- lib/genre.rb
+++ lib/genre.rb
@@ -1,1 +1,30 @@
-# code genre here
+class Genre
+  attr_accessor :name
+  @all = []
+
+  def initialize
+    self.class.all << self
+  end
+
+  class << self
+    attr_reader :all
+  end
+
+  def songs
+    Song.all.select { |song| song.genre == self }
+  end
+
+  def artists
+    Artist.all.select do |artist|
+      artist.songs.any? { |song| song.genre == self }
+    end
+  end
+
+  def self.reset_genres
+    all.clear
+  end
+
+  def self.count
+    all.count
+  end
+end
